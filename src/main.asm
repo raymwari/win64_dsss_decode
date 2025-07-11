53244bf548039ee60885bbb72dfd80f7305cabd6
raymwari <mwari.ray@gmail.com>
2025-07-10 23:14:44 -0700

    Update main.asm

--- src/main.asm
+++ src/main.asm
@@ -1,12 +1,12 @@
 section .data
   file_name db "out.mp3", 0
   OF_PROMPT equ 0x00002000
-  MAX_JPEG_SIZE equ 966656
+  MAX_FILE_SIZE equ 966656
   MAX_BIN_SIZE equ 9666560
 
 section .bss
   ofstruct resb 136
-  file_read_buf resb MAX_JPEG_SIZE
+  file_read_buf resb MAX_FILE_SIZE
   read resb 1
   bin_buf resb MAX_BIN_SIZE
 
