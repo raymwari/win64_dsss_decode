5b07ba80ced34c4e1072e97d21b2bcf7ec31145b
fallen8192 <mwari.ray@gmail.com>
2025-08-14 00:39:00 -0700

    Delete src directory

--- src/run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-if exist main.exe (del main.exe)
-nasm -f win64 main.asm
-nasm -f win64 dec.asm
-golink /console /entry _main main.obj dec.obj ecodes.obj kernel32.dll
-main.exe
-if exist main.exe (del main.obj dec.obj)
-if exist main.exe (main.exe)
